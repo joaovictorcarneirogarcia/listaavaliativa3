601807339e92a6f5279f8ad851b35792ca49191b
João Victor <142359695+joaovictorcarneirogarcia@users.noreply.github.com>
2023-11-28 20:24:51 -0300

    Add files via upload

---
+++ Questão 3.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int main() {
+    char placa[10];
+    char dia[20];
+
+    // Leitura da placa e do dia da semana
+    scanf("%s %s", placa, dia);
+
+    // Verificação do formato da placa
+    int formatoInvalido = 0;
+    if (strlen(placa) == 8) {
+        if (!isalpha(placa[0]) || !isalpha(placa[1]) || !isalpha(placa[2]) || placa[3]!='-' || !isdigit(placa[4]) || !isdigit(placa[5]) || !isdigit(placa[6]) || !isdigit(placa[7])) {
+            formatoInvalido = 1;
+        }
+    } else if (strlen(placa) == 7) {
+        if (!isalpha(placa[0]) || !isalpha(placa[1]) || !isalpha(placa[2]) || !isdigit(placa[3]) || !isalpha(placa[4]) || !isdigit(placa[5]) || !isdigit(placa[6])) {
+            formatoInvalido = 1;
+        }
+    } else {
+        formatoInvalido = 1;
+    }
+
+    // Verificação do formato do dia da semana
+    int diaInvalido = 0;
+    if (strcmp(dia, "SEGUNDA-FEIRA") != 0 && strcmp(dia, "TERCA-FEIRA") != 0 && strcmp(dia, "QUARTA-FEIRA") != 0 && strcmp(dia, "QUINTA-FEIRA") != 0 && strcmp(dia, "SEXTA-FEIRA") != 0 && strcmp(dia, "SABADO") != 0 && strcmp(dia, "DOMINGO") != 0) {
+        diaInvalido = 1;
+    }
+
+    // Verificação e saída conforme as regras
+    if (formatoInvalido) {
+        printf("Placa invalida\n");
+    } else if (diaInvalido) {
+        printf("Dia da semana invalido\n");
+    } else {
+        int ultimoDigito = placa[strlen(placa) - 1] - '0';
+
+        if ((strcmp(dia, "SEGUNDA-FEIRA") == 0 && (ultimoDigito == 0 || ultimoDigito == 1)) ||
+            (strcmp(dia, "TERCA-FEIRA") == 0 && (ultimoDigito == 2 || ultimoDigito == 3)) ||
+            (strcmp(dia, "QUARTA-FEIRA") == 0 && (ultimoDigito == 4 || ultimoDigito == 5)) ||
+            (strcmp(dia, "QUINTA-FEIRA") == 0 && (ultimoDigito == 6 || ultimoDigito == 7)) ||
+            (strcmp(dia, "SEXTA-FEIRA") == 0 && (ultimoDigito == 8 || ultimoDigito == 9))) {
+            printf("%s nao pode circular %s\n", placa, dia);
+        } else if (strcmp(dia, "SABADO") == 0 || strcmp(dia, "DOMINGO") == 0) {
+            printf("Nao ha proibicao no fim de semana\n");
+        } else {
+        printf("%s pode circular %s\n", placa, dia);
+        }
+    }
+
+    return 0;
+}
